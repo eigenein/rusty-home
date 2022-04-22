2161dc6cee983f6eed54fa827524bf06d9ce0ffc
Pavel Perestoronin <eigenein@gmail.com>
2022-04-23 00:05:46 +0200

    New: add the common deps

--- rusty-tado/src/main.rs
+++ rusty-tado/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tracing::info;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::Registry::default()
+        .with(sentry::integrations::tracing::layer())
+        .with(EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?)
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    info!("hello");
+    Ok(())
 }
