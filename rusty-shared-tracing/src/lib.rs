c6818b4d367f5d6ef52735db5d9d95906b6e3fe7
Pavel Perestoronin <eigenein@gmail.com>
2022-04-23 00:14:48 +0200

    New: move tracing to the new `rusty-shared-tracing` crate

---
+++ rusty-shared-tracing/src/lib.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
+pub fn init() -> Result<()> {
+    tracing_subscriber::Registry::default()
+        .with(sentry::integrations::tracing::layer())
+        .with(EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?)
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    Ok(())
+}
--- rusty-tado/src/main.rs
+++ rusty-tado/src/main.rs
@@ -1,16 +1,9 @@
 use anyhow::Result;
 use tracing::info;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::EnvFilter;
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::Registry::default()
-        .with(sentry::integrations::tracing::layer())
-        .with(EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?)
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    rusty_shared_tracing::init()?;
     info!("hello");
     Ok(())
 }
