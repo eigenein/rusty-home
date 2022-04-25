0a8efb41265ad95cf3422011a9101e0e5bcb8a91
Pavel Perestoronin <eigenein@gmail.com>
2022-04-25 22:46:53 +0200

    New: rate-limit the heartbeat

--- rusty-shared-opts/src/heartbeat.rs
+++ rusty-shared-opts/src/heartbeat.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
+use async_std::sync::Mutex;
 use clap::Parser;
 use reqwest::{Client, Url};
+use std::time::Instant;
 use tracing::{debug, instrument, warn};
 
 #[derive(Parser)]
@@ -16,25 +18,47 @@ impl Opts {
             warn!("heartbeat URL is not specified, heartbeat is disabled");
         }
         self.url
-            .map(|url| Ok(Heartbeat(Some((Client::new(), url.parse()?)))))
-            .unwrap_or_else(|| Ok(Heartbeat(None)))
+            .map(|url| Ok(Heartbeat::new(Some((Client::new(), url.parse()?)))))
+            .unwrap_or_else(|| Ok(Heartbeat::new(None)))
     }
 }
 
-pub struct Heartbeat(Option<(Client, Url)>);
+pub struct Heartbeat {
+    endpoint: Option<(Client, Url)>,
+    last_beat_timestamp: Mutex<Instant>,
+}
 
 impl Heartbeat {
+    const INTERVAL: ::std::time::Duration = ::std::time::Duration::from_secs(60);
+
+    pub fn new(endpoint: Option<(Client, Url)>) -> Self {
+        Self {
+            endpoint,
+            last_beat_timestamp: Mutex::new(Instant::now() - Self::INTERVAL),
+        }
+    }
+
     #[instrument(level = "debug", skip_all)]
     pub async fn send(&self) {
-        let (client, url) = match &self.0 {
-            Some(client) => client,
+        let mut last_beat_timestamp = self.last_beat_timestamp.lock().await;
+        if last_beat_timestamp.elapsed() < Self::INTERVAL {
+            debug!(
+                elapsed = format!("{:?}", last_beat_timestamp.elapsed()).as_str(),
+                "heartbeat is rate-limited",
+            );
+            return;
+        }
+
+        match &self.endpoint {
+            Some((client, url)) => {
+                if let Err(error) = client.post(url.clone()).send().await {
+                    warn!("heartbeat error: {:#}", error);
+                }
+            }
             None => {
-                debug!("ignoring the heartbeat");
-                return;
+                debug!("heartbeat is disabled");
             }
         };
-        if let Err(error) = client.post(url.clone()).send().await {
-            warn!("heartbeat error: {:#}", error);
-        }
+        *last_beat_timestamp = Instant::now();
     }
 }
--- rusty-tractive/README.md
+++ rusty-tractive/README.md
@@ -4,10 +4,10 @@ Connects Rusty Home to [Tractive](https://tractive.com/).
 
 ## Heartbeat
 
-The heartbeat is being sent every time the microservice receives a message from Tractive, and that is normally every **5 seconds**. However, the keep-alive TTL is **5 minutes** as reported by Tractive.
+The heartbeat is being sent every time the microservice receives a message from Tractive, but no more than once a minute. Normally Tractive sends a keep-alive message every **5 seconds**. However, the keep-alive TTL is **5 minutes** as reported by Tractive. To summarize, the heartbeat is **expected every 1-5 minutes**.
 
 ## [Better Uptime](https://betteruptime.com/)
 
 | Expect a heartbeat every | with a grace period of  |
 |--------------------------|-------------------------|
-| 30 seconds               | 5 minutes               |
+| 1 minute                 | 5 minutes               |
