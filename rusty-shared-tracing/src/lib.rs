d914db2cfcd452b02843b4eed2e86325c31953dc
Pavel Perestoronin <eigenein@gmail.com>
2022-05-02 14:01:25 +0200

    Fix: Sentry should capture `DEBUG` spans as well

--- rusty-shared-tracing/src/lib.rs
+++ rusty-shared-tracing/src/lib.rs
@@ -1,13 +1,30 @@
 use anyhow::Result;
+use sentry::integrations::tracing::EventFilter;
+use tracing::Level;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{EnvFilter, Layer};
 
 pub fn init() -> Result<()> {
+    let sentry_layer = sentry::integrations::tracing::layer()
+        .event_filter(|metadata| match metadata.level() {
+            &Level::ERROR | &Level::WARN => EventFilter::Event,
+            &Level::INFO | &Level::DEBUG => EventFilter::Breadcrumb,
+            _ => EventFilter::Ignore,
+        })
+        .span_filter(|metadata| {
+            matches!(
+                metadata.level(),
+                &Level::ERROR | &Level::WARN | &Level::INFO | &Level::DEBUG
+            )
+        });
+    let format_layer = tracing_subscriber::fmt::layer()
+        .without_time()
+        .with_filter(EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?);
+
     tracing_subscriber::Registry::default()
-        .with(sentry::integrations::tracing::layer())
-        .with(EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?)
-        .with(tracing_subscriber::fmt::layer().without_time())
+        .with(sentry_layer)
+        .with(format_layer)
         .init();
     Ok(())
 }
